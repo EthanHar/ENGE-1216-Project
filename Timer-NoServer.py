8430197d13b70cfa32a09dae32ff9038e22e4e72
EthanHar <129694492+EthanHar@users.noreply.github.com>
2023-04-02 21:22:14 -0400

    Update and rename Timer.py to Timer-NoServer.py

--- Timer-NoServer.py
+++ Timer-NoServer.py
@@ -5,9 +5,11 @@ def timer(wake_up):
     now = datetime.now()
     current_time = str(now.strftime("%H : %M : %S"))
     current_time = current_time[:7]
+    #finds and initializes current_time to the current time
     
     
     while (current_time!=wake_up):
+        #continues checking if it is the correct time
         time.sleep(5)
         now = datetime.now()
         current_time = str(now.strftime("%H : %M : %S"))
